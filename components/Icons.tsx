d598308e359021fdbd0693383d64927d1307a1fc
Kamil Mrowka <kamil@chicos.life>
2024-11-06 19:16:42 +0100

    Refactor CircleIcon component in Icons.tsx to use Path instead of Circle, add fillInner and fillOuter props for more customization options

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,28 +1,21 @@
 import {colorNames} from '@/constants/theme';
-import Svg, {Circle} from 'react-native-svg';
+import Svg, {Circle, Ellipse, Path} from 'react-native-svg';
 
 export const CircleIcon = ({
-  fill = 'none', // No fill for an empty circle
-  stroke = colorNames.softLavender, // Color for the outline
-  strokeWidth = 2, // Width of the outline
+  fillInner = 'none',
+  fillOuter = 'none', // No fill for an empty circle
   width = 35,
   height = 35,
   ...rest
 }) => (
-  <Svg
-    width={width}
-    height={height}
-    viewBox="0 0 35 35"
-    fill="none"
-    {...rest} // Spread any other props
-  >
-    <Circle
-      cx="17.5"
-      cy="17.5"
-      r="16.5" // Reduced radius slightly to fit inside SVG bounds with stroke
-      fill={fill}
-      stroke={stroke}
-      strokeWidth={strokeWidth}
+  <Svg width={width} height={height} viewBox="0 0 35 35" {...rest}>
+    <Path
+      d="M34 17.5C34 27.165 26.3888 35 17 35C7.61116 35 0 27.165 0 17.5C0 7.83502 7.61116 0 17 0C26.3888 0 34 7.83502 34 17.5Z"
+      fill={fillOuter}
+    />
+    <Path
+      d="M28 17.4572C28 23.2562 23.0751 27.9572 17 27.9572C10.9249 27.9572 6 23.2562 6 17.4572C6 11.6582 10.9249 6.95719 17 6.95719C23.0751 6.95719 28 11.6582 28 17.4572Z"
+      fill={fillInner}
     />
   </Svg>
 );
