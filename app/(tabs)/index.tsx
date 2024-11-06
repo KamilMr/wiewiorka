f66e3b18afce8a51cd29fbf6cd04cfe6238836c2
Kamil Mrowka <kamil@chicos.life>
2024-11-06 19:58:40 +0100

    upd_2024-11-06_0878a729

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,22 @@
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 import {Text} from '@/components';
+import {View} from 'react-native';
+import {useAppTheme} from '@/constants/theme';
 
 const Home = () => {
+  const t = useAppTheme();
   return (
-    <SafeAreaView>
-      <Text>Hellog index</Text>
+    <SafeAreaView style={{backgroundColor: t.colors.white}}>
+      <View
+        style={{
+          height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: t.colors.white,
+        }}>
+        <Text>Tu coś będzie.</Text>
+      </View>
     </SafeAreaView>
   );
 };
