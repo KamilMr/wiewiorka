7e33f5259206cdd3152acc5f9b87bcf56c0c416e
kamil <kamil@chicos.life>
2025-01-19 15:38:13 +0100

    Refactor CustomTextInput imports

--- components/CustomTextInput.tsx
+++ components/CustomTextInput.tsx
@@ -1,26 +1,25 @@
-import {sizes} from '@/constants/theme';
-import React, { forwardRef } from 'react';
+import React, {forwardRef} from 'react';
 import {StyleSheet} from 'react-native';
+
 import {TextInput, TextInputProps} from 'react-native-paper';
 
-const CustomTextInput = forwardRef<typeof TextInput, TextInputProps>(({
-  onChangeText,
-  value,
-  style,
-  ...props
-}, ref) => {
-  return (
-    <TextInput
-      onChangeText={onChangeText}
-      dense
-      mode="outlined"
-      value={value}
-      ref={ref}
-      style={[styles.root, style]}
-      {...props}
-    />
-  );
-});
+import {sizes} from '@/constants/theme';
+
+const CustomTextInput = forwardRef<typeof TextInput, TextInputProps>(
+  ({onChangeText, value, style, ...props}, ref) => {
+    return (
+      <TextInput
+        onChangeText={onChangeText}
+        dense
+        mode="outlined"
+        value={value}
+        ref={ref}
+        style={[styles.root, style]}
+        {...props}
+      />
+    );
+  },
+);
 
 const styles = StyleSheet.create({
   root: {
